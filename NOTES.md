686deb3ac956dde6e95b89eeb4fb02db64205f8c
Emmanuel Orozco <emmanuel@laboratoria.la>
2018-02-20 16:53:50 -0600

    changes to uggly version

---
+++ NOTES.md
@@ -0,0 +1,4 @@
+1. forEach en lugar de for
+2. Reduce para las notas
+3. Filter para filtar estudiantes activas
+4. Programacion imperativa vs declarativa
--- js/app.js
+++ js/app.js
@@ -1,17 +1,43 @@
 var data = [
-  { url: "http://lorempixel.com/300/300/sports/", name: "Manu" },
-  { url: "http://lorempixel.com/300/300/people/", name: "Meme" },
-  { url: "http://lorempixel.com/300/300/city/", name: "Lucile" },
-  { url: "http://lorempixel.com/300/300/nature/", name: "Karla" },
-  { url: "http://lorempixel.com/300/300/cats/", name: "Silvana" },
-  { url: "http://lorempixel.com/300/300/dogs/", name: "Amalia" }
+  {
+    url: "http://dummyimage.com/300/09f/",
+    name: "Vale",
+    grades: [4, 3, 1],
+    active: false
+  },
+  {
+    url: "http://dummyimage.com/300/13f/",
+    name: "Meme",
+    grades: [10, 8, 9],
+    active: false
+  },
+  {
+    url: "http://dummyimage.com/300/39f/",
+    name: "Lucile",
+    grades: [8, 7, 2],
+    active: true
+  },
+  {
+    url: "http://dummyimage.com/300/89f/",
+    name: "Karla",
+    grades: [5, 8, 7],
+    active: true
+  },
+  {
+    url: "http://dummyimage.com/300/19f/",
+    name: "Silvana",
+    grades: [3, 4, 10],
+    active: false
+  },
+  {
+    url: "http://dummyimage.com/300/04f/",
+    name: "Amalia",
+    grades: [8, 6, 10],
+    active: false
+  }
 ];
 
 document.getElementById("el-select").addEventListener("change", pasarAlgo);
-
-document
-  .getElementById("agregar-imagen")
-  .addEventListener("click", crearContenedor);
 
 function crearContenedor() {
   for (var i = 0; i < data.length; i++) {
@@ -25,8 +51,18 @@ function crearContenedor() {
     var divContenedor = document.createElement("div");
     divContenedor.className = "contenedor-imagenes";
 
+    var divNota = document.createElement("p");
+    var acumuladoDeNota = 0;
+    for (var j = 0; j < data[i].grades.length; j++) {
+      acumuladoDeNota += data[i].grades[j];
+    }
+    var nota = acumuladoDeNota / 3;
+    divNota.innerText = Math.floor(nota);
+    console.log(nota);
+
     divContenedor.appendChild(imagenNueva);
     divContenedor.appendChild(pNuevo);
+    divContenedor.appendChild(divNota);
     document.body.appendChild(divContenedor);
   }
 }
@@ -35,34 +71,49 @@ function pasarAlgo(event) {
   var selectedIndex = event.target.selectedIndex;
   var filtroAAplicar = event.target[selectedIndex].dataset.filtro;
   if (filtroAAplicar == "gray") {
-    cambiarAGray();
+    var imagenesACambiar = document.getElementsByTagName("img");
+    for (var i = 0; i < imagenesACambiar.length; i++) {
+      imagenesACambiar[i].className = "gray";
+    }
   } else if (filtroAAplicar == "sepia") {
-    cambiarASepia();
+    var imagenesACambiar = document.getElementsByTagName("img");
+    for (var i = 0; i < imagenesACambiar.length; i++) {
+      imagenesACambiar[i].className = "sepia";
+    }
   } else if (filtroAAplicar == "nada") {
-    quitarFiltro();
+    var imagenesACambiar = document.getElementsByTagName("img");
+    for (var i = 0; i < imagenesACambiar.length; i++) {
+      imagenesACambiar[i].style.filter = "none";
+    }
   }
 }
 
-function quitarFiltro() {
-  var imagenesACambiar = document.getElementsByTagName("img");
-  console.log(imagenesACambiar);
-  for (var i = 0; i < imagenesACambiar.length; i++) {
-    imagenesACambiar[i].style.filter = "none";
+var filtarActivas = document.getElementById("filtrarActivas");
+filtrarActivas.addEventListener("click", filtrarActivasFunction);
+
+var filtrarInactivas = document.getElementById("filtrarInactivas");
+filtrarInactivas.addEventListener("click", filtrarInactivasFunction);
+
+function filtrarActivasFunction() {
+  var filtradasActivas = [];
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].active == true) {
+      console.log(data[i].active);
+      filtradasActivas.push(data[i]);
+    }
   }
+  console.log(filtradasActivas);
+
+  // todo pintar estudiantes activas
 }
 
-function cambiarAGray() {
-  var imagenesACambiar = document.getElementsByTagName("img");
-  console.log(imagenesACambiar);
-  for (var i = 0; i < imagenesACambiar.length; i++) {
-    imagenesACambiar[i].className = "gray";
+function filtrarInactivasFunction() {
+  var filtradas = [];
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].active == false) {
+      filtradas.push(data[i]);
+    }
   }
-}
-
-function cambiarASepia() {
-  var imagenesACambiar = document.getElementsByTagName("img");
-  console.log(imagenesACambiar);
-  for (var i = 0; i < imagenesACambiar.length; i++) {
-    imagenesACambiar[i].className = "sepia";
-  }
+  console.log(filtradas);
+  // todo pintar inactivas
 }
